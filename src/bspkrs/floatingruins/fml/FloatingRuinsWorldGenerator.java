8dff73fe6b7ac18fa3680292c52e694cb874fdb5
bspkrs <bspkrs@gmail.com>
2013-11-02 01:12:16 -0400

    changed to delayed action ticker to give villages a chance to instantiate before checking for them

--- src/bspkrs/floatingruins/fml/FloatingRuinsWorldGenerator.java
+++ src/bspkrs/floatingruins/fml/FloatingRuinsWorldGenerator.java
@@ -1,11 +1,14 @@
 package bspkrs.floatingruins.fml;
 
+import java.util.EnumSet;
 import java.util.Random;
 
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
-import bspkrs.floatingruins.FloatingRuins;
 import cpw.mods.fml.common.IWorldGenerator;
+import cpw.mods.fml.common.TickType;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
 
 public class FloatingRuinsWorldGenerator implements IWorldGenerator
 {
@@ -15,7 +18,7 @@ public class FloatingRuinsWorldGenerator implements IWorldGenerator
     @Override
     public void generate(Random random, int x, int z, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
-        FloatingRuins.generateSurface(world, random, x << 4, z << 4, true);
+        TickRegistry.registerTickHandler(new DelayedWorldGenTicker(EnumSet.of(TickType.SERVER), 10, world, random, x << 4, z << 4), Side.SERVER);
     }
     
 }
