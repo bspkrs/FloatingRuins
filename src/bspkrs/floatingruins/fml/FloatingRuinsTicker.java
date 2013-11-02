e31fee4563683fc87cc55b7edcd7cf320a0f74bd
bspkrs <bspkrs@gmail.com>
2013-11-02 01:10:56 -0400

    switched to new TickerBase superclass

--- src/bspkrs/floatingruins/fml/FloatingRuinsTicker.java
+++ src/bspkrs/floatingruins/fml/FloatingRuinsTicker.java
@@ -4,49 +4,24 @@ import java.util.EnumSet;
 
 import net.minecraft.client.Minecraft;
 import bspkrs.bspkrscore.fml.bspkrsCoreMod;
+import bspkrs.fml.util.TickerBase;
 import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class FloatingRuinsTicker implements ITickHandler
+public class FloatingRuinsTicker extends TickerBase
 {
-    private Minecraft         mcClient;
-    
-    private EnumSet<TickType> tickTypes = EnumSet.noneOf(TickType.class);
+    private Minecraft mcClient;
     
     public FloatingRuinsTicker(EnumSet<TickType> tickTypes)
     {
-        this.tickTypes = tickTypes;
+        super(tickTypes);
         this.mcClient = FMLClientHandler.instance().getClient();
     }
     
     @Override
-    public void tickStart(EnumSet<TickType> tickTypes, Object... tickData)
-    {
-        tick(tickTypes, true);
-    }
-    
-    @Override
-    public void tickEnd(EnumSet<TickType> tickTypes, Object... tickData)
-    {
-        tick(tickTypes, false);
-    }
-    
-    private void tick(EnumSet<TickType> tickTypes, boolean isStart)
-    {
-        for (TickType tickType : tickTypes)
-        {
-            if (!onTick(tickType, isStart))
-            {
-                this.tickTypes.remove(tickType);
-                this.tickTypes.removeAll(tickType.partnerTicks());
-            }
-        }
-    }
-    
     public boolean onTick(TickType tick, boolean isStart)
     {
         if (isStart)
@@ -64,12 +39,6 @@ public class FloatingRuinsTicker implements ITickHandler
         }
         
         return true;
-    }
-    
-    @Override
-    public EnumSet<TickType> ticks()
-    {
-        return this.tickTypes;
     }
     
     @Override
