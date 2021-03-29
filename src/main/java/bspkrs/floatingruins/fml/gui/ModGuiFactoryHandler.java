5f76224ffce4cfcb195bc945329b8e85a0376bb5
Aaronstar-McClure <jfeldt19@gmail.com>
2021-03-28 21:55:10 -0400

    clean modGuiFactoryHandler.java and delete GuiFRConfig.java
    see comments

--- src/main/java/bspkrs/floatingruins/fml/gui/ModGuiFactoryHandler.java
+++ src/main/java/bspkrs/floatingruins/fml/gui/ModGuiFactoryHandler.java
@@ -2,9 +2,13 @@ package bspkrs.floatingruins.fml.gui;
 
 import java.util.Set;
 
+import bspkrs.floatingruins.fml.Reference;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
+import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
 
 public class ModGuiFactoryHandler implements IModGuiFactory
 {
@@ -15,10 +19,32 @@ public class ModGuiFactoryHandler implements IModGuiFactory
     }
 
     @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass()
+    public boolean hasConfigGui()
     {
-        return GuiFRConfig.class;
+    	return true;
     }
+    
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen)
+    {
+        return new ConfigGui(parentScreen);
+    }
+
+    public static class ConfigGui extends GuiConfig
+    {
+        public ConfigGui(GuiScreen parent)
+        {
+        	super(parent, (new ConfigElement(Reference.config.getCategory(Configuration.CATEGORY_GENERAL))).getChildElements(),
+                    Reference.MODID, false, false, GuiConfig.getAbridgedConfigPath(Reference.config.toString()));
+        }
+    }
+    
+    //unfortunantly this was removed. it seems the contents of guiFRConfig needs to be moved in here...
+//    @Override
+//    public Class<? extends GuiScreen> mainConfigGuiClass()
+//    {
+//        return GuiFRConfig.class;
+//    }
 
     @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
@@ -26,9 +52,11 @@ public class ModGuiFactoryHandler implements IModGuiFactory
         return null;
     }
 
-    @Override
-    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
-    {
-        return null;
-    }
+    // unable to find a use for this method, cant find any reference to getHandlerFor() in old versions of forge
+    // deprecated in 1.11.2?
+//    @Override
+//    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
+//    {
+//        return null;
+//    }
 }
